518902871581c13570642448fee2d6b3fbfcbf36
supersmarty07 <elohistsachet007@gmail.com>
2021-10-08 22:16:55 +1100

    Creating this App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StatisticsComponent } from './details/statistics/statistics.component';
+
+import { CountryComponent } from './details/country/country.component';
+import { SearchBarComponent } from './searchComponent/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatisticsComponent,
+    CountryComponent,
+    SearchBarComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
